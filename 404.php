ec8bfa74821fcf49d7d1e55760343038cba4cf6b
Iddah <iddahkamau@gmail.com>
2013-08-14 16:20:59 +0300

    Empty theme

---
+++ 404.php
@@ -0,0 +1,23 @@
+<?php
+get_header();
+// informea_debug_wp_request();
+?>
+<div>
+    &nbsp;
+</div>
+<div>
+    <h1>Page gone missing</h1>
+    <div>
+        <p>
+            While this is embarassing, we've recently updated the website and some things may have broken.
+            <br />
+            Please visit our <a href="<?php bloginfo('url'); ?>">home page</a> and start fresh.
+        </p>
+        <p>
+            We'll do better next time!
+        </p>
+    </div>
+    <img src="http://www.informea.org/wp-content/uploads/2013/04/robot.jpg" />
+</div>
+<?php
+get_footer();
