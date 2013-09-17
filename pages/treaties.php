658827a77594bb16287e4cc5a59413a3abb10643
Cristian Romanescu <cristian@romanescu.ro>
2013-09-17 10:29:10 +0300

    Reformat code, replace tabs with spaces

--- pages/treaties.php
+++ pages/treaties.php
@@ -15,49 +15,51 @@ $regions = i3_treaty_regions_in_use();
 <h1><?php the_title(); ?></h1>
 
 <table id="treaties-table" class="table">
-	<thead>
-		<tr>
-			<th colspan="2">Showing <?php echo $count; ?> out of <?php echo $count_total; ?> treaties</th>
-			<th class="hidden-phone">
-				<span class="placeholder">All Topics</span>
-				<div class="dropdown inline filter">
-					<button class="btn dropdown-toggle" data-toggle="dropdown">
-						<span class="caret"></span>
-					</button>
-					<ul class="dropdown-menu pull-right" role="menu">
-						<?php foreach($topics as $row): ?>
-						<li><a tabindex="-1" href="#<?php echo $row; ?>"><?php echo $row; ?></a></li>
-						<?php endforeach; ?>
-					</ul>
-				</div>
-			</th>
-			<th class="hidden-phone">
-				<span class="placeholder">Coverage</span>
-				<div class="dropdown inline filter">
-					<button class="btn dropdown-toggle" data-toggle="dropdown">
-						<span class="caret"></span>
-					</button>
-					<ul class="dropdown-menu pull-right" role="menu">
-						<?php foreach($regions as $row): ?>
-						<li><a tabindex="-1" href="#<?php echo $row->name; ?>"><?php echo $row->name; ?></a></li>
-						<?php endforeach; ?>
-					</ul>
-				</div>
-			</th>
-			<th class="hidden-phone">
-				<span>Year</span>
-			</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php foreach($treaties as $row): ?>
-		<tr>
-			<td><img src="<?php echo $row->logo_medium; ?>" alt="<?php echo $row->short_title; ?> logo"></td>
-			<td class="treaty-title"><h2><?php echo $row->short_title; ?></h2></td>
-			<td class="hidden-phone"><?php i3_treaty_format_topic($row); ?>&nbsp;</td>
-			<td class="hidden-phone"><?php i3_treaty_format_region($row); ?></td>
-			<td class="hidden-phone"><?php echo $row->year; ?></td>
-		</tr>
-		<?php endforeach; ?>
-	</tbody>
+    <thead>
+    <tr>
+        <th colspan="2">Showing <?php echo $count; ?> out of <?php echo $count_total; ?> treaties</th>
+        <th class="hidden-phone">
+            <span class="placeholder">All Topics</span>
+
+            <div class="dropdown inline filter">
+                <button class="btn dropdown-toggle" data-toggle="dropdown">
+                    <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu pull-right" role="menu">
+                    <?php foreach ($topics as $row): ?>
+                        <li><a tabindex="-1" href="#<?php echo $row; ?>"><?php echo $row; ?></a></li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        </th>
+        <th class="hidden-phone">
+            <span class="placeholder">Coverage</span>
+
+            <div class="dropdown inline filter">
+                <button class="btn dropdown-toggle" data-toggle="dropdown">
+                    <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu pull-right" role="menu">
+                    <?php foreach ($regions as $row): ?>
+                        <li><a tabindex="-1" href="#<?php echo $row->name; ?>"><?php echo $row->name; ?></a></li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        </th>
+        <th class="hidden-phone">
+            <span>Year</span>
+        </th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($treaties as $row): ?>
+        <tr>
+            <td><img src="<?php echo $row->logo_medium; ?>" alt="<?php echo $row->short_title; ?> logo"></td>
+            <td class="treaty-title"><h2><?php echo $row->short_title; ?></h2></td>
+            <td class="hidden-phone"><?php i3_treaty_format_topic($row); ?>&nbsp;</td>
+            <td class="hidden-phone"><?php i3_treaty_format_region($row); ?></td>
+            <td class="hidden-phone"><?php echo $row->year; ?></td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
 </table>
