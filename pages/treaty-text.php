8858c3bae3a1906d4a27552023e360c674ebf59e
Cristian Romanescu <cristian@romanescu.ro>
2013-09-20 14:01:35 +0300

    Added template for treaty texT

---
+++ pages/treaty-text.php
@@ -0,0 +1,30 @@
+<?php
+global $treaty, $organization;
+?>
+<div class="row">
+    <div class="span3">
+        <div class="toolbar">
+            <strong>Table of Contents</strong>
+        </div>
+        <ul class="nav nav-list">
+            <li><a href="#">Target</a></li>
+            <li><a href="#">Target</a></li>
+            <li><a href="#">Target</a></li>
+            <li><a href="#">Target</a></li>
+            <li><a href="#">Target</a></li>
+            <li><a href="#">Target</a></li>
+        </ul>
+    </div>
+    <div class="treaty-text-container span9">
+        <div class="toolbar">
+            <strong>Treaty Text</strong>
+            <div class="pull-right">
+                <a href="#"><i class="icon-print"></i> Print</a> <a href="#"><i class="icon-url"></i> Cite/Link</a>
+            </div>
+        </div>
+        <div class="treaty-text">
+            <h1>Title</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc scelerisque risus eu neque accumsan imperdiet. Quisque blandit suscipit ante sit amet vehicula. Curabitur at felis scelerisque, venenatis ante nec, dictum diam. Quisque sit amet eleifend nisi, non gravida tellus. Vestibulum sem risus, rhoncus et mollis at, molestie vel dolor. Ut scelerisque arcu eu odio pharetra, quis condimentum enim gravida.</p>
+        </div>
+    </div>
+</div>
