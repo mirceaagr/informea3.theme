095cc5de1813485832b34f298046eed58620d44e
Cristian Romanescu <cristian@romanescu.ro>
2013-10-01 10:59:15 +0300

    Removed tabs from file

--- pages/treaty-toolbar-tpl.php
+++ pages/treaty-toolbar-tpl.php
@@ -1,7 +1,7 @@
 <div class="treaty-text-toolbar well hidden-phone clearfix">
-	<div class="pull-left">
-    	<strong>Treaty Text</strong>
-	</div>
+    <div class="pull-left">
+        <strong>Treaty Text</strong>
+    </div>
     <div class="pull-right">
         <button class="btn btn-inline"><i class="icon-print"></i> Print</button>
         <button class="btn btn-inline"><i class="icon-link"></i> Cite/Link</button>
