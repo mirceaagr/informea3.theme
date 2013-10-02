d3d1c104ce61f8960dd1aa07921712122486a887
Cristian Romanescu <cristian@romanescu.ro>
2013-10-02 13:16:29 +0300

    Created the script to handle Ajax requests

---
+++ functions_informea_ajax.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Author: Cristian Romanescu <cristi _at_ eaudeweb dot ro>
+ * Created: 201310021250
+ */
+
+function informea_ajaxurl() {
+?>
+    <script type="text/javascript">
+        var ajaxurl = '<?php echo admin_url('admin-ajax.php'); ?>';
+    </script>
+<?php
+}
