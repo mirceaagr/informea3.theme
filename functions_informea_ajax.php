08cf4543a2fbe555e86b47e9b3c2c7c35be48ed9
Cristian Romanescu <cristian@romanescu.ro>
2013-10-03 10:42:48 +0300

    refs #272 - Request is parsed with HTTPFoundation

--- functions_informea_ajax.php
+++ functions_informea_ajax.php
@@ -4,16 +4,20 @@
  * Created: 201310021250
  */
 
+use Symfony\Component\HttpFoundation\Request;
+
 add_action('wp_ajax_nopriv_get_nfp_for_treaty_country', 'informea_get_nfp_for_treaty_country_callback');
 add_action('wp_ajax_get_nfp_for_treaty_country', 'informea_get_nfp_for_treaty_country_callback');
 function informea_get_nfp_for_treaty_country_callback() {
-    $id_treaty = $_GET['id_treaty'];
-    $iso = $_GET['iso'];
 
+    informea_load_symfony();
+
+    $request = Request::createFromGlobals();
+    $id_treaty = $request->get('id_treaty');
+    $iso = $request->get('iso');
     $contacts = InforMEA::get_treaty_country_nfp($id_treaty, $iso);
     foreach($contacts as $row) {
         i3_treaty_nfp_format($row);
     }
     die();
 }
-
