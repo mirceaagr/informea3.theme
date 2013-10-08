9cd15fe608d1ee5401504a32bbb89c901037c9af
Cristian Romanescu <cristian@romanescu.ro>
2013-10-08 11:39:30 +0300

    refs #280 - Reorganized code in page-treaties

--- page-treaties.php
+++ page-treaties.php
@@ -3,8 +3,10 @@
 $request = WordPressHttpRequestFactory::createFromGlobals();
 $odata_name = $request->get('odata_name');
 $view = $request->get('view');
+$modal = $request->get('display') == 'modal';
 $treaty = InforMEA::get_treaty_by_odata_name($odata_name);
 $act = $request->get('act');
+$is_print = $act == 'print';
 
 if($treaty) {
     add_action('wp_enqueue_scripts', function() {
@@ -43,30 +45,37 @@ add_filter('the_breadcrumb', 'informea_treaties_breadcrumbtrail');
 
 
 if (have_posts()) : while (have_posts()) : the_post();
-    if($view == 'treaties') {
-        echo InforMEATemplate::treaties();
-    } else if($view == 'treaty') {
-        /** Add the scrollspy classes to the body tag */
-        function informea_treaties_body_attributes($c) {
-            $c[] = '" data-spy="scroll" data-target=".scrollspy';
-            return $c;
-        }
-        add_filter('body_class','informea_treaties_body_attributes');
-        // Inject ajaxurl into the front-end scripts as config object
-        add_action('wp_enqueue_scripts',
-            function() {
-                wp_enqueue_script('informea-treaties');
-                wp_localize_script('informea-treaties', 'i3_config_ajax', array('ajaxurl' => admin_url('admin-ajax.php')));
+    switch($view) {
+        case 'treaties':
+            echo InforMEATemplate::treaties();
+            break;
+        case 'treaty':
+            /** Add the scrollspy classes to the body tag */
+            function informea_treaties_body_attributes($c) {
+                $c[] = '" data-spy="scroll" data-target=".scrollspy';
+                return $c;
             }
-        );
-        echo InforMEATemplate::treaty($treaty);
-    } else if($view == 'text') { // Ajax call
-        $organization = InforMEA::get_organization($treaty->id_organization);
-        $modal = $request->get('display') == 'modal';
-        $print = $act == 'print';
-        if($print) {
-            add_action('wp_enqueue_scripts', function() { wp_enqueue_style('informea-print'); });
-        }
-        echo InforMEATemplate::treaty_text_viewer($treaty, $organization, $modal, $print);
+            add_filter('body_class','informea_treaties_body_attributes');
+            // Inject ajaxurl into the front-end scripts as config object
+            add_action('wp_enqueue_scripts',
+                function() {
+                    wp_enqueue_script('informea-treaties');
+                    wp_localize_script('informea-treaties', 'i3_config_ajax', array('ajaxurl' => admin_url('admin-ajax.php')));
+                }
+            );
+            echo InforMEATemplate::treaty($treaty);
+            break;
+        case 'text': // Normal page and Ajax call
+            $organization = InforMEA::get_organization($treaty->id_organization);
+            if($is_print) {
+                add_action('wp_enqueue_scripts', function() { wp_enqueue_style('informea-print'); });
+            }
+            echo InforMEATemplate::treaty_text_viewer($treaty, $organization, $modal, $is_print);
+            break;
+        case 'decision':
+            $id_decision = $request->get('id_decision');
+            break;
+        default:
+            die("There nothing to do here. Nothing!");
     }
 endwhile; endif;
