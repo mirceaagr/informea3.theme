5be0a201f5e2ae133b63928687d59c6e8078e802
Dragos Dumitrescu <dumitrescu.dragos1@gmail.com>
2013-10-28 16:42:00 +0200

    ref #284 script for ajax focal points

---
+++ scripts/country.js
@@ -0,0 +1,23 @@
+jQuery('document').ready(function() {
+    jQuery('#treaty-nfp-country-select').on('change', function(){
+        get_country_nfp_from_treaty(jQuery(this).val());
+    });
+});
+
+function get_country_nfp_from_treaty(id_treaty){
+    jQuery.ajax({
+            url: i3_config_ajax.ajaxurl,
+            data: {
+                dataType: 'html',
+                action: 'get_country_nfp_from_treaty',
+                id_treaty: id_treaty,
+                id_country: jQuery('#id_country').val()
+            }
+        }).done(function(resp) {
+            jQuery('#focal-point-list').html(resp);
+            jQuery('#treaty-nfp-country-count').html(jQuery('#focal-point-list>li.focal-point').length);
+            var $option = jQuery('#treaty-nfp-country-select').find(':selected');
+            jQuery('#country-treaty-nfp-link').attr('href', $option.attr('data-url'));
+            jQuery('#country-nfp-treaty-logo').attr('src', $option.attr('data-src'));
+        });
+}
